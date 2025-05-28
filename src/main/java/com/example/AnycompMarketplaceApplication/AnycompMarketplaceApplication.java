b5728129539b74fbd3d2b479db1d8ac68a215a51
Your Name <hreddyjulakanti007@gmail.com>
2025-05-28 10:50:51 +0530

    duplicate

--- src/main/java/com/example/AnycompMarketplaceApplication/AnycompMarketplaceApplication.java
+++ src/main/java/com/example/AnycompMarketplaceApplication/AnycompMarketplaceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnycompMarketplaceApplication {
 	public static void main(String[] args) {
+		int x=34;
+
 		SpringApplication.run(AnycompMarketplaceApplication.class, args);
 	}
 }
